4a4ba11bdc2736bb1106268689cd2c6c644833f4
Luke Westby <luke.westby@raise.com>
2016-01-23 11:01:06 -0600

    initial commit

---
+++ README.md
@@ -0,0 +1,32 @@
+# elm-http-extra
+
+Extra helpers for more easily building Http requests that require greater
+configuration than what is provided by `elm-http` out of the box.
+
+## Example
+
+```elm
+import Time
+import Http
+import Http.Extra as HttpExtra
+import Json.Decode as Json
+
+
+itemsDecoder : Json.Decoder (List String)
+itemsDecoder =
+  Json.list Json.string
+
+
+addItem : String -> Task Http.Error (List String)
+addItem item =
+  HttpExtra.post "http://example.com/api/items"
+    |> withBody (Http.string "{ \"item\": \"" ++ item ++ "\" }")
+    |> withHeader ("Content-Type", "application/json")
+    |> withTimeout (10 * Time.second)
+    |> withCredentials
+    |> send itemsDecoder
+```
+
+## Contributing
+
+I'm happy to receive any feedback and thinking about additional features. Any input and pull requests are very welcome and encouraged. If you'd like to help or have ideas, get in touch with me at @luke_dot_js on Twitter or @luke in the elmlang Slack!
---
+++ src/Http/Extra.elm
@@ -0,0 +1,201 @@
+module Http.Extra
+  ( RequestBuilder, get, post, put, patch, delete
+  , withHeader, withHeaders, withBody
+  , withTimeout, withStartHandler, withProgressHandler, withMimeType, withCredentials
+  , send
+  ) where
+
+{-| Extra helpers for more easily building Http requests that require greater
+configuration than what is provided by `elm-http` out of the box.
+
+
+# Start a request
+@docs RequestBuilder, get, post, put, patch, delete
+
+# Configure request properties
+@docs withHeader, withHeaders, withBody
+
+# Configure settings
+@docs withTimeout, withStartHandler, withProgressHandler, withMimeType, withCredentials
+
+# Send the request
+@docs send
+-}
+
+import Task exposing (Task)
+import Maybe exposing (Maybe(..))
+import Time exposing (Time)
+import Json.Decode as Json
+import Http
+
+
+{-| An type for chaining request configuration
+-}
+type RequestBuilder =
+  RequestBuilder Http.Request Http.Settings
+
+
+requestWithVerbAndUrl : String -> String -> RequestBuilder
+requestWithVerbAndUrl verb url =
+  RequestBuilder
+    { verb = verb
+    , url = url
+    , headers = []
+    , body = Http.empty
+    }
+    Http.defaultSettings
+
+
+mapRequest : (Http.Request -> Http.Request) -> RequestBuilder -> RequestBuilder
+mapRequest updater (RequestBuilder request settings) =
+  RequestBuilder
+    (updater request)
+    (settings)
+
+
+mapSettings : (Http.Settings -> Http.Settings) -> RequestBuilder -> RequestBuilder
+mapSettings updater (RequestBuilder request settings) =
+  RequestBuilder
+    (request)
+    (updater settings)
+
+
+{-| Start building a GET request with a given URL
+
+    get "https://example.com/api/items/1"
+-}
+get : String -> RequestBuilder
+get =
+  requestWithVerbAndUrl "GET"
+
+
+{-| Start building a POST request with a given URL
+
+    post "https://example.com/api/items"
+-}
+post : String -> RequestBuilder
+post =
+  requestWithVerbAndUrl "POST"
+
+
+{-| Start building a PUT request with a given URL
+
+    post "https://example.com/api/items/1"
+-}
+put : String -> RequestBuilder
+put =
+  requestWithVerbAndUrl "PUT"
+
+
+{-| Start building a PATCH request with a given URL
+
+    patch "https://example.com/api/items/1"
+-}
+patch : String -> RequestBuilder
+patch =
+  requestWithVerbAndUrl "PATCH"
+
+
+{-| Start building a DELETE request with a given URL
+
+    delete "https://example.com/api/items/1"
+-}
+delete : String -> RequestBuilder
+delete =
+  requestWithVerbAndUrl "DELETE"
+
+
+{-| Add a single header to a request
+
+    get "https://example.com/api/items/1"
+      |> withHeader ("Content-Type", "application/json")
+-}
+withHeader : (String, String) -> RequestBuilder -> RequestBuilder
+withHeader header =
+  mapRequest <| \request -> { request | headers = header :: request.headers }
+
+
+{-| Add many headers to a request
+
+    get "https://example.com/api/items/1"
+      |> withHeaders [("Content-Type", "application/json"), ("Accept", "application/json")]
+-}
+withHeaders : List (String, String) -> RequestBuilder -> RequestBuilder
+withHeaders headers =
+  mapRequest <| \request -> { request | headers = headers ++ request.headers }
+
+
+{-| Add a body to a request for requests that allow bodies.
+
+    post "https://example.com/api/items/1"
+      |> withHeader ("Content-Type", "application/json")
+      |> withBody (Http.string """{ "sortBy": "coolness", "take": 10 }""")
+-}
+withBody : Http.Body -> RequestBuilder -> RequestBuilder
+withBody body =
+  mapRequest <| \request -> { request | body = body }
+
+
+{-| Set the `timeout` setting on the request
+
+    get "https://example.com/api/items/1"
+      |> withTimeout (10 * Time.second)
+-}
+withTimeout : Time -> RequestBuilder -> RequestBuilder
+withTimeout timeout =
+  mapSettings <| \settings -> { settings | timeout = timeout }
+
+
+{-| Set the `onStart` setting on the request
+
+    get "https://example.com/api/items/1"
+      |> withStartHandler (onStartTask)
+-}
+withStartHandler : Task () () -> RequestBuilder -> RequestBuilder
+withStartHandler task =
+  mapSettings <| \settings -> { settings | onStart = Just task }
+
+
+{-| Set the `onProgress` setting on the request
+
+    get "https://example.com/api/items/1"
+      |> withProgressHandler (onProgressHandler)
+-}
+withProgressHandler : (Maybe { loaded : Int, total : Int } -> Task () ()) -> RequestBuilder -> RequestBuilder
+withProgressHandler progressHandler =
+  mapSettings <| \settings -> { settings | onProgress = Just progressHandler }
+
+
+{-| Set the desired type of the response for the request, works via
+[`XMLHttpRequest#overrideMimeType()`](https://developer.mozilla.org/en-US/docs/Web/API/XMLHttpRequest#overrideMimeType())
+
+    get "https://example.com/api/items/1"
+      |> withMimeType (onProgressHandler)
+-}
+withMimeType : String -> RequestBuilder -> RequestBuilder
+withMimeType mimeType =
+  mapSettings <| \settings -> { settings | desiredResponseType = Just mimeType }
+
+
+{-| Set the `withCredentials` flag on the request to True. Works via
+[`XMLHttpRequest#withCredentials`](https://developer.mozilla.org/en-US/docs/Web/API/XMLHttpRequest/withCredentials)
+
+    get "https://example.com/api/items/1"
+      |> withCredentials
+-}
+withCredentials : RequestBuilder -> RequestBuilder
+withCredentials =
+  mapSettings <| \settings -> { settings | withCredentials = True }
+
+
+{-| Once you're finished building up a request, send it with a given decoder
+
+    get "https://example.com/api/items"
+      |> withHeader ("Content-Type", "application/json")
+      |> withTimeout (10 * Time.second)
+      |> send (Json.Decoder.list Json.Decoder.string)
+-}
+send : Json.Decoder a -> RequestBuilder -> Task Http.Error a
+send decoder (RequestBuilder request settings) =
+  Http.send settings request
+    |> Http.fromJson decoder
