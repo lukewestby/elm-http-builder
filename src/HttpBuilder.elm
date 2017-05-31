5130cadb567eb30670f3f7be017c3e49dc5281d6
Kevin Ndung'u <kevgathuku@gmail.com>
2017-05-31 14:21:15 +0300

    Fix formatting for withBody docs

--- src/HttpBuilder.elm
+++ src/HttpBuilder.elm
@@ -174,6 +174,7 @@ withHeaders headerPairs builder =
 
 
 {-| Add an Http.Body to the request
+
     post "https://example.com/api/save-text"
         |> withBody (Http.stringBody "text/plain" "Hello!")
 -}
